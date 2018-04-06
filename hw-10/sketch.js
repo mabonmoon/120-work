47749df7b91e02308905d3ab903c8dd0acbe30e1
Kimberly Pierson <kimberlycpierson@gmail.com>
2018-04-05 21:36:03 -0600

    edits

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,99 +1,86 @@
-/*var nums = [100, 25, 46, 72];
-var words = ["witch", "wizard", "mountain","moon"];
+
+//creating array
+
+var words = ["sun", "mercury", "venus","earth","mars","jupiter","saturn","uranus","neptune"];
 var index = 0;
-var num = 23;
-*/
 
-var bubbles = [];
-var img;
-
-function preload(){
-img = loadImage('images/hand.png');
-
-}
-
+//creating an empty array
+var planets = [];
+//creating my canvas
 function setup(){
-  createCanvas(600,400);
-  for( var i = 0; i < 5; i++){
-  bubbles[i] = new Bubble(width/2,height/2);
-
+createCanvas(windowWidth,windowHeight);
+//putting objects into my array + creating a loop for many planets
+  for( var i = 0; i < 1000; i++){
+//new Planet creates new object + attaches properties defined in below function returning my object that was created in the below function Planet
+  planets[i] = new Planet(width/2,height/2);
 }
+
+
 }
 function draw(){
 background(0);
-for (var i=0; i < bubbles.length; i++){
-bubbles[i].move();
-bubbles[i].display();
-  }
 
-//removing shapes from my array
-  if(bubbles.length>10){
-    bubbles.splice(0,1)
-  }
+
+//displaying my word array randomizing where they appear
+fill(255);
+textSize(50);
+text(words[index], random(width), random(height));
+index=index+1;
+
+if (index==words.length){
+
+index=0;
 }
 
 
-//making a constructor function
 
-function Bubble(x,y) {
+
+//looping through the length of my array
+//no matter how many planets in array loop will display every object until it reaches the array's length
+for (var i=0; i < planets.length; i++){
+planets[i].move();
+planets[i].display();
+  }
+
+
+
+//removing shapes from my array with splice function
+  if(planets.length>1000){
+    planets.splice(0,1)
+  }
+
+}
+
+
+//defining my constructor function for Planet object and creating parameters
+
+function Planet(x,y) {
+  //attaching property to function
 this.x = x;
 this.y = y;
-//the way I want my objects to look
+//the way I want my objects to look attaching display function
 this.display = function() {
-  //stroke(random(0,255));
-  //fill(random(255),random(255),random(255));
-  image(img,this.x,this.y);
-  //ellipse(this.x, this.y, 24, 24);
-  //line(this.x,this.y,random(width), random(height));
-}
-
-this.move = function() {
-  this.x = this.x + random(-1, 1);
-  this.y = this.y + random(-1, 1);
-
-  }
-
-}
-//adding shapes to where the mouse is pressed
-function mousePressed() {
-bubbles.push(new Bubble(mouseX,mouseY));
-
-}
-
-/*for(var i=0; i < 4; i++){
-stroke(255);
-noFill();
-  ellipse(i*100 +100,200,nums[i], nums[i]);
-}
-
-
-
-/*fill(255);
-textSize(32);
-text(words[index], random(width), random(height));
-frameRate(12)*/
-
-//}
-
-/*function mousePressed(){
-  index = index + 1;
-//resetting my index so that the code cycles through
-  if(index==words.length){
-
-    index=0;
-  }
-}
-
-x: width/2,
-y: height/2,
-display: function() {
+  //randomizing colors
   stroke(random(0,255));
-  noFill();
-  ellipse(this.x, this.y, 24, 24);
-  line(this.x,this.y,random(width), random(height));
-},
-move: function() {
-  this.x = this.x + random(-1, 1);
-  this.y = this.y + random(-1, 1);
-}*/
-//}
+  strokeWeight(.2);
+  //slowing my frame rate so its less chaotic!
+  frameRate(3);
+    line(this.x,this.y,random(width), random(height));
+    //further intense randomization of RGB values
+    fill(random(255),random(255),random(255), 50);
+  ellipse(this.x, this.y, 100, 100);
+
+
+}
+//creating movement for my object
+this.move = function() {
+  this.x = this.x + random(-2, 2);
+  this.y = this.y + random(-2, 2);
+
+  }
+
+}
+//adding more planets to where the mouse is pressed with push function
+function mousePressed() {
+planets.push(new Planet(mouseX,mouseY));
+}
