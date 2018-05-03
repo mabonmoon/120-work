2a0d65673016b2cd3e6f0d3be5170183c1d60ec9
Kimberly Pierson <kimberlycpierson@gmail.com>
2018-05-03 14:54:04 -0600

    test image

---
+++ Final Project/sketch.js
@@ -0,0 +1,20 @@
+
+
+var img;
+function preload() {
+
+ // preload() runs once
+  img = loadImage('images/birdlady.png');
+
+
+function setup() {
+  createCanvas(1280, 1080);
+}
+
+function draw() {
+//  background(bg);
+show() {
+  image(img, 0, 0);
+
+}
+}
